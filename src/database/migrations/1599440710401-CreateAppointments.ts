189d942664876513a05e5b784f2919bd672e51d3
thomasneo <thomaslossio@hotmail.com>
2020-09-07 15:13:44 -0300

    Fix uuid generation for postgres

--- src/database/migrations/1599440710401-CreateAppointments.ts
+++ src/database/migrations/1599440710401-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1599440710401
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
